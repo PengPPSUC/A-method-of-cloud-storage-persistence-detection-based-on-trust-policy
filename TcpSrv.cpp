f6ccb6d253ca1441cddc6d214df2deee1bb73d74
PengPPSUC <cppsujianpeng@126.com>
2017-05-06 11:34:08 +0800

    Update TcpSrv.cpp
    增加相关的文件操作，文件序号操作

--- TcpSrv.cpp
+++ TcpSrv.cpp
@@ -3,12 +3,17 @@
 #pragma warning( disable : 4786)
 #endif
 #include <WinSock2.h>
+#include <string>
 #include <stdio.h>
+#include <fstream>
 #include "md5class.h"
 #include <vector>
-#include <string>
 #include <time.inl>
-
+#include <iostream>
+#include <cassert>
+#include <atlstr.h>
+#include <iosfwd>
+#include <sstream>
 using namespace std;
 
 void main()
@@ -32,7 +37,9 @@ void main()
 			WSACleanup( );
 			return; 
 	}
-
+	
+	char *FileRead(const char *textPath,char *textBuff);
+	string int_to_String(int n);
 	CMD5 md5;
 
 	//SOCKET sockSrv = socket(AF_INET,SOCK_STREAM,0);//TCP
@@ -44,13 +51,29 @@ void main()
 
 	bind(sockSrv,(SOCKADDR*)&addrSrv,sizeof(SOCKADDR));
 
-	char recvBuf[100];
-	char sendBuf[100];
-	char tempBuf[100];
 
 	SOCKADDR_IN addrClient;
 	int len = sizeof(SOCKADDR);
-
+	char recvBuf[100];
+	char sendBuf[100];
+	char tempBuf[100];
+	char *tempTextBuff="";
+	char *textBuff="";
+	string fileMainPath = "C:/Users/Hasee/Desktop/";
+	int textBloclNum=1;
+	string textName="";
+	textName=int_to_String(textBloclNum);
+	//printf("%s\n",textName.c_str());
+	string textform=".txt";
+	string TrueTextPath=fileMainPath+textName+textform;
+	const char *textPath=TrueTextPath.c_str();
+	printf("The path is %s \n",textPath);
+	textBuff=FileRead(TrueTextPath.c_str(),tempTextBuff);
+	printf("the text is :%s.\n",textBuff);
+	//printf("The text is:%s\n",readLineTemp);
+	//const char *readLineChar =readLine.c_str();
+	//md5.setPlainText(readLineChar);
+	//printf("The text's md5 is : %s\n",md5.getMD5Digest());
 	while (1)
 	{
 		recvfrom(sockSrv,recvBuf,100,0,(SOCKADDR*)&addrClient,&len);
@@ -92,3 +115,33 @@ void main()
 	*/
 }
 
+
+
+char *FileRead(const char *textPath,char *textBuff)
+{
+	FILE *pFile;
+	if (NULL==(pFile=fopen(textPath,"r")))
+	{
+		printf("Error,can't open file!");
+	} 
+	else
+	{
+		fseek(pFile,0,SEEK_END);
+		int len=ftell(pFile);
+		//printf("the text's len is :%d \n",len);
+		textBuff=new char[len+1];
+		rewind(pFile);
+		fread(textBuff,1,len,pFile);
+		textBuff[len]=0;
+		fclose(pFile);
+	}
+	return textBuff;
+}
+
+string int_to_String(int n)
+{
+	string fileName="";
+	fileName+=to_string((long double)n);
+
+	return fileName;
+}
